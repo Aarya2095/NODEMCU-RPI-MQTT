34935d76913cb6d182d5ebd441a316d622fadd74
Arya2095 <maiteyee.rao@gmail.com>
2018-10-03 20:56:11 +0530

    no message

---
+++ BEPROJECT1.ino
@@ -0,0 +1,110 @@
+
+#include <ESP8266WiFi.h> 
+#include "Adafruit_MQTT.h" 
+#include "Adafruit_MQTT_Client.h" 
+#include "MQ135.h"
+#define ANALOGPIN A0    //  
+#define RZERO 79609.2    //  Define RZERO Calibration Value
+//Wifi Access Point
+#define WLAN_SSID       "Tech_D0017220" 
+#define WLAN_PASS       "YRJUUTJG" 
+#define MQTT_SERVER      "192.168.0.31"  // give static address
+#define MQTT_PORT         1883                    
+#define MQTT_USERNAME    "" 
+#define MQTT_PASSWORD    "" 
+
+MQ135 gasSensor = MQ135(ANALOGPIN);
+
+// Create an esp8266 wificlient  to connect to the MQTT server. 
+WiFiClient client; 
+
+// Setup the mqtt client class
+
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD); 
+/****************************** Feeds ***************************************/ 
+// Setup a feed  for publishing. 
+
+Adafruit_MQTT_Publish pi_led = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME "/leds/pi"); 
+
+// Setup a feed for subscribtion
+Adafruit_MQTT_Subscribe esp8266_led = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "/leds/esp8266"); 
+/*************************** Sketch Code ************************************/ 
+void MQTT_connect(); 
+
+void setup() { 
+ Serial.begin(9600); 
+ delay(10); 
+ Serial.println(F("RPi-ESP-MQTT")); 
+ 
+ // Connect to wifi access point. 
+ Serial.println(); Serial.println(); 
+ Serial.print("Connecting to "); 
+ Serial.println(WLAN_SSID); 
+ WiFi.begin(WLAN_SSID, WLAN_PASS); 
+ while (WiFi.status() != WL_CONNECTED) { 
+   delay(500); 
+   Serial.print("."); 
+ } 
+ Serial.println(); 
+ Serial.println("WiFi connected"); 
+ Serial.println("IP address: "); Serial.println(WiFi.localIP()); 
+ 
+ // Setup MQTT subscription for esp8266_led feed. 
+ mqtt.subscribe(&esp8266_led); 
+ float rzero = gasSensor.getRZero();
+  delay(3000);
+} 
+int initValue= 0;
+
+void loop() { 
+   
+ MQTT_connect(); 
+
+ Adafruit_MQTT_Subscribe *subscription; 
+ while ((subscription = mqtt.readSubscription())) { 
+   if (subscription == &esp8266_led) { 
+     Serial.print(F("Got: ")); 
+     Serial.println((char *)esp8266_led.lastread); 
+
+     /*  */
+ } 
+ }
+
+
+  
+    
+    float ppm = gasSensor.getPPM();
+  delay(1000);
+  
+  Serial.print("CO2 ppm value : ");
+  Serial.println(ppm);
+    
+    if (! pi_led.publish(ppm)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+   }
+    
+}
+// Function to connect and reconnect as necessary to the MQTT server. 
+void MQTT_connect() { 
+ int8_t ret; 
+ // Stop if already connected. 
+ if (mqtt.connected()) { 
+   return; 
+ } 
+ Serial.print("Connecting to MQTT... "); 
+ uint8_t retries = 3; 
+ while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected 
+      Serial.println(mqtt.connectErrorString(ret)); 
+      Serial.println("Retrying MQTT connection in 5 seconds..."); 
+      mqtt.disconnect(); 
+      delay(5000);  // wait 5 seconds 
+      retries--; 
+      if (retries == 0) { 
+        // basically die and wait for WDT to reset me 
+        while (1); 
+      } 
+ } 
+ Serial.println("MQTT Connected!"); 
+}  
